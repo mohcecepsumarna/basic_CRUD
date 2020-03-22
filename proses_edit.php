ed20f40670306a9105fa0827a9b7977dc4d5e021
Moh Cecep Sumarna <mohcecepsumarna@gmail.com>
2020-03-22 22:44:01 +0700

    --- Auto-Git Commit ---

---
+++ proses_edit.php
@@ -0,0 +1,8 @@
+<?php
+//memanggil file koneksi.php untuk melakukan koneksi database
+include 'koneksi.php';
+
+    //membuat variable untuk menampung data dari form
+    $id     = $_POST['id'];
+
+?>
